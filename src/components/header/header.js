3dea194f96be3654a55fadd139a2c459bd0c74c1
Leandro Javier Schmidt <leandro.schmidt90@gmail.com>
2021-04-18 01:43:43 -0300

    Installed initial packages / assets. Finished header mock up

---
+++ src/components/header/header.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Search from "../../styles/assets/ic_Search.png";
+import Logo from "../../styles/assets/Logo_ML.png";
+
+import "./header.scss";
+
+function Header() {
+  return (
+    <div id="header">
+      <img src={Logo} alt="logo" />
+      <form>
+        <input placeholder="Buscar productos, marcas y más..."></input>
+        <button>
+          <img src={Search} alt="logo" />
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default Header;
